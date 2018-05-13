e8c1daa38dd6076f00c716897222823c52fe3a0d
zioroboco <zioroboco@gmail.com>
2018-05-13 20:50:31 +1000

    Initial commit

---
+++ Assets/Inputs.cs
@@ -0,0 +1,22 @@
+﻿using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+
+public class Inputs : MonoBehaviour {
+    
+    public static Inputs Instance { get; private set; }
+    public IObservable<Vector2> Movement { get; private set; }
+
+    private void Awake() {
+        
+        Instance = this;
+        
+        Movement = this.FixedUpdateAsObservable()
+            .Select(_ => {
+                var x = Input.GetAxis("Horizontal");
+                var y = Input.GetAxis("Vertical");
+                return new Vector2(x, y).normalized;
+            });
+    }
+    
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using UniRx;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float WalkSpeed = 1f;
+    private CharacterController _character;
+    
+    private void Start()
+    {
+        var inputs = Inputs.Instance;
+
+        _character = GetComponent<CharacterController>();
+
+        inputs.Movement
+            .Where(v => v != Vector2.zero)
+            .Subscribe(inputMovement =>
+            {
+                var inputVelocity = inputMovement * WalkSpeed;
+                var playerVelocity =
+                    inputVelocity.x * transform.right +
+                    inputVelocity.y * transform.forward;
+                var distance = playerVelocity * Time.fixedDeltaTime;
+                _character.Move(distance);
+            }).AddTo(this);
+
+    }
+}
